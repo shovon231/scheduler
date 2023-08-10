072aa65023cdb9f0d41391cdeeb0307b13effca9
shovon231 <shovon231@gmail.com>
2023-08-09 21:24:09 -0400

    adding the daylistitem and check with storybook

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,8 +1,15 @@
 import React from "react";
+import classNames from "classnames/bind";
+import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
+  const dayListClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
   return (
     <li
+      className={dayListClass}
       onClick={() => {
         props.setDay(props.name);
       }}
