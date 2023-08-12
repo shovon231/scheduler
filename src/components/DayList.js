03357451f067667d0d4aaacf4783a80b6282719a
shovon231 <shovon231@gmail.com>
2023-08-11 20:19:35 -0400

    adding interviewList and adding to the storybook for functionality

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import DayListItem from "./DayListItem";
 
-export default function (props) {
+
+export default function DayList(props) {
   const days = props.days.map((day) => {
     return (
       <DayListItem
