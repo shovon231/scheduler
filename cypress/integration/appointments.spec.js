0d785564149c5a760d72eb33ef545dcfd8911b83
shovon231 <shovon231@gmail.com>
2023-09-15 15:12:27 -0400

    adding edit and delete test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -27,4 +27,43 @@ describe("Appointments", () => {
     // Verify that the appointment card displays the interviewer's name ("Sylvia Palmer")
     cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  // Test case: should edit an interview
+  it("should edit an interview", () => {
+    // Click the "Edit" button for the first appointment slot (using 'force' because it might not be initially visible)
+    cy.get("[alt=Edit]").first().click({ force: true });
+
+    // Clear the student's name and type a new name ("Lydia Miller-Jones")
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    // Select a different interviewer (in this case, "Tori Malcolm")
+    cy.get("[alt='Tori Malcolm']").click();
+
+    // Click the "Save" button to save the edited interview
+    cy.contains("Save").click();
+
+    // Verify that the appointment card displays the edited student's name ("Lydia Miller-Jones")
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+
+    // Verify that the appointment card displays the new interviewer's name ("Tori Malcolm")
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+
+  // Test case: should cancel an interview
+  it("should cancel an interview", () => {
+    // Click the "Delete" button for the first appointment slot (using 'force' because it might not be initially visible)
+    cy.get("[alt=Delete]").click({ force: true });
+
+    // Click the "Confirm" button to confirm the deletion
+    cy.contains("Confirm").click();
+
+    // Check for the "Deleting" text to exist and then disappear
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    // Verify that the appointment card for "Archie Cohen" does not exist (indicating cancellation)
+    cy.contains(".appointment__card--show", "Archie Cohen").should("not.exist");
+  });
 });
